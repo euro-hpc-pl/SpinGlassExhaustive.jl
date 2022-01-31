0c3bef6a3a49cdecbaacc7ecdd0f8177070d5c9f
dk <kurzyk.dariusz@gmail.com>
2022-01-31 03:43:54 +0100

    cleaning

--- benchmarks/bf_bench.jl
+++ benchmarks/bf_bench.jl
@@ -1,40 +1,40 @@
-using SpinGlassNetworks, LightGraphs, SpinGlassExhaustive
-using CUDA, LinearAlgebra
-using Bits
+# using SpinGlassNetworks, LightGraphs, SpinGlassExhaustive
+# using CUDA, LinearAlgebra
+# using Bits
 
-function bench_cpu(instance::String, max_states::Int=100)
-    m = 2
-    n = 2
-    t = 24
+# function bench_cpu(instance::String, max_states::Int=100)
+#     m = 2
+#     n = 2
+#     t = 24
 
-    ig = ising_graph(instance)
-    cl = split_into_clusters(ig, super_square_lattice((m, n, t)))
-    @time sp = brute_force(cl[1, 1], num_states=max_states)
-    sp
-end
+#     ig = ising_graph(instance)
+#     cl = split_into_clusters(ig, super_square_lattice((m, n, t)))
+#     @time sp = brute_force(cl[1, 1], num_states=max_states)
+#     sp
+# end
 
 
 
-function bench_gpu(instance::String, max_states::Int=100)
-    m = 2
-    n = 2
-    t = 24
+# function bench_gpu(instance::String, max_states::Int=100)
+#     m = 2
+#     n = 2
+#     t = 24
 
-    ig = ising_graph(instance)
-    cl = split_into_clusters(ig, super_square_lattice((m, n, t)))
-    @time sp = brute_force(cl[1, 1], :GPU; num_states=max_states)
-    sp
-end
+#     ig = ising_graph(instance)
+#     cl = split_into_clusters(ig, super_square_lattice((m, n, t)))
+#     @time sp = brute_force(cl[1, 1], :GPU; num_states=max_states)
+#     sp
+# end
 
-println("*** CPU ***")
-sp_cpu = bench_cpu("$(@__DIR__)/pegasus_droplets/2_2_3_00.txt")
-sp_cpu = bench_cpu("$(@__DIR__)/pegasus_droplets/2_2_3_00.txt")
-println("*** GPU ***")
-sp_gpu = bench_gpu("$(@__DIR__)/pegasus_droplets/2_2_3_00.txt")
-sp_gpu = bench_gpu("$(@__DIR__)/pegasus_droplets/2_2_3_00.txt")
+# println("*** CPU ***")
+# sp_cpu = bench_cpu("$(@__DIR__)/pegasus_droplets/2_2_3_00.txt")
+# sp_cpu = bench_cpu("$(@__DIR__)/pegasus_droplets/2_2_3_00.txt")
+# println("*** GPU ***")
+# sp_gpu = bench_gpu("$(@__DIR__)/pegasus_droplets/2_2_3_00.txt")
+# sp_gpu = bench_gpu("$(@__DIR__)/pegasus_droplets/2_2_3_00.txt")
 
-@assert sp_gpu.energies ≈ sp_cpu.energies
-@assert all(
-    sp_gpu.states[i] == sp_cpu.states[i]
-    for i ∈ 1:size(sp_cpu.states, 1)
-)
+# @assert sp_gpu.energies ≈ sp_cpu.energies
+# @assert all(
+#     sp_gpu.states[i] == sp_cpu.states[i]
+#     for i ∈ 1:size(sp_cpu.states, 1)
+# )
--- src/SpinGlassExhaustive.jl
+++ src/SpinGlassExhaustive.jl
@@ -1,5 +1,23 @@
+# module SpinGlassExhaustive
+
+# greet() = print("Hello World!")
+
+# end # module
+
+"""
+Main module for `SpinGlassExhaustive.jl` -- a Julia package for brute-force spin-glass problems with CUDA.
+"""
+
 module SpinGlassExhaustive
+using CUDA
+using BenchmarkTools
+using StaticArrays
+using Setfield
+using DocStringExtensions
+eval(Expr(:export, names(SpinGlassExhaustive)...))
 
-greet() = print("Hello World!")
+include("bitonicsort.jl")
+include("ising.jl")
+include("utils.jl")
 
-end # module
+end
