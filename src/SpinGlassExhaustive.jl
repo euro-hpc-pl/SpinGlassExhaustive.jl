19f0ff5c5db8845678ddec4c014a5d5a033d99f8
dk <kurzyk.dariusz@gmail.com>
2022-09-27 17:10:33 +0200

    refaktoring SpinGlassExhaustive.jl

--- src/SpinGlassExhaustive.jl
+++ src/SpinGlassExhaustive.jl
@@ -3,17 +3,17 @@ Main module for `SpinGlassExhaustive.jl` -- a Julia package for brute-force spin
 """
 
 module SpinGlassExhaustive
-using SpinGlassNetworks
-using LabelledGraphs
-using CUDA
-using BenchmarkTools
-using StaticArrays
-using Setfield
-using DocStringExtensions
-eval(Expr(:export, names(SpinGlassExhaustive)...))
+    using SpinGlassNetworks
+    using LabelledGraphs
+    using CUDA
+    using BenchmarkTools
+    using StaticArrays
+    using Setfield
+    using DocStringExtensions
 
-include("bitonicsort.jl")
-include("ising.jl")
-include("utils.jl")
+
+    include("bitonicsort.jl")
+    include("ising.jl")
+    include("utils.jl")
 
 end
